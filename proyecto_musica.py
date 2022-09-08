f637bd2c3663313a1fa1e3db9f978cbfdedd3e45
angieriosc <111139966+angieriosc@users.noreply.github.com>
2022-09-07 22:07:42 -0500

    Add files via upload

--- proyecto_musica.py
+++ proyecto_musica.py
@@ -3,7 +3,6 @@ from tkinter.ttk import Combobox
 from tkinter import messagebox
 from tkinter import ttk
 
-
 #funciones para flujo de ventanas
 
 def popingles():
@@ -59,34 +58,27 @@ def sig_r():
     ventana_pi.deiconify()
     
 #Colocación dentro de la ventanas
+    
 def colocacion():
-
 
     intro.place(x=185, y=30)
     intro2.place(x=85, y=50)
-
     nombre.place(x=80, y=100, width=450, height=25)
     respuesta_nombre.place(x=130, y=140, width=320, height=25)
 
     pregunta1.place(x=80, y=190, width=450, height=25)
-
-
     pregunta2.place(x=80, y=280, width=450, height=25)
-
-
     pregunta3.place(x=80, y=370, width=450, height=25)
     pregunta3_2.place(x=80, y=395, width=450, height=25)
-
-
     pregunta4.place(x=80, y=490, width=450, height=25)
     pregunta5.place(x=80, y=580, width=450, height=25)
 
     final.place(x=195, y=700, width=210, height=25)
-    
     regresar_inicio.place(x=20, y=700, width=140, height=25)
-
     siguiente_nivel.place(x=445, y=700, width=140, height=25)
 
+#Funciones para mostrar calificación en la pantalla
+    
 def mensaje_pi():
 #contador puntaje
  respuesta1_pi=str(opciones1_pi.get())
@@ -155,7 +147,6 @@ def mensaje_m():
  respuesta4_m=str(opciones4_m.get())
  respuesta5_m=str(opciones5_m.get())
 
-
  puntaje_m=int(0)
  if respuesta1_m=="Dear Evan Hansen":
     puntaje_m=puntaje_m + 1
@@ -176,6 +167,7 @@ def mensaje_m():
  nombre=str(respuesta_nombre.get())
  print(messagebox.askretrycancel(message=("Gracias " + nombre +" por conterstar, tu calificación es", cal_m), title="Calificación"))
 
+
 def mensaje_r():
 #contador puntaje
  respuesta1_r=str(opciones1_r.get())
@@ -184,8 +176,8 @@ def mensaje_r():
  respuesta4_r=str(opciones4_r.get())
  respuesta5_r=str(opciones5_r.get())
 
-
  puntaje_r=int(0)
+ 
  if respuesta1_r=="Queen":
     puntaje_r=puntaje_r + 1
 
@@ -204,8 +196,7 @@ def mensaje_r():
  cal_r=puntaje_r/.5
  nombre=str(respuesta_nombre.get())
  print(messagebox.askretrycancel(message=("Gracias " + nombre +" por conterstar, tu calificación es", cal_r), title="Calificación"))
-    
-        
+          
 
 ###ventana principal
 ventana=Tk()
@@ -225,24 +216,15 @@ cuadro.config(bg="lightgrey")
 cuadro.config(bd=20)
 
 pop_ingles=Button(cuadro, text="Pop Ingles", fg="aquamarine", font=("Roboto Cn",34),command=popingles)
-
 pop_español=Button(cuadro, text="Pop Español", fg="orange", font=("Roboto Cn",34),command=popespañol)
-
 musical=Button(cuadro, text="Musicales",fg="hot pink", font=("Roboto Cn",34),command=musicales)
-
 rock=Button(cuadro, text="Pop Español",fg="darkviolet", font=("Roboto Cn",34),command=rock)
 
 pop_ingles.place(x=180, y=190, width=450, height=60)
-
 pop_español.place(x=180, y=310, width=450, height=60)
-
 musical.place(x=180, y=430, width=450, height=60)
-
 rock.place(x=180, y=560, width=450, height=60)
                 
-
-
-
 
 ###ventana secundaria pop ingles
  
@@ -262,7 +244,6 @@ canvas_pi.config(bg="white")
 cuadro_pi.config(bg="aquamarine2")
 cuadro_pi.config(bd=20)
 
-
 intro=Label(cuadro_pi, text = "Contesta las preguntas sobre pop")
 intro.config(bg="white", fg="aquamarine")
 intro2=Label(cuadro_pi, text = "Al finalizar dar click en el botón final para conocer tu resultado")
@@ -271,9 +252,7 @@ nombre=Label(cuadro_pi, text="Ingresa tu nombre:")
 nombre.config(bg="aquamarine", fg="white")
 respuesta_nombre=Entry(cuadro_pi)
 
-
 #Uso de label para mostrar preguntas
-
 pregunta1=Label(cuadro_pi, text="¿Qué banda canta la canción “The Scientist”?")
 pregunta1.config(bg="aquamarine", fg="white")
 pregunta2=Label(cuadro_pi, text="¿Qué canción pertenece a Shawn Mendes”?")
@@ -313,15 +292,7 @@ final.config(relief="raised")
 colocacion()
 
 
-
-
-
-
-
 ###ventana secundaria pop español
-
-#contador puntaje
-
 
 ventana_pe=Toplevel(ventana)
 ventana_pe.title("Pop Español")
@@ -347,9 +318,7 @@ nombre=Label(cuadro_pe, text="Ingresa tu nombre:")
 nombre.config(bg="orange", fg="white")
 respuesta_nombre=Entry(cuadro_pe)
 
-
-#Uso de combobox para desplegar opciones de respuesta
-
+#Uso de label para mostrar pregunta 
 pregunta1=Label(cuadro_pe, text="¿Qué banda canta la canción “Alejate de Mi”?")
 pregunta1.config(bg="orange", fg="white")
 pregunta2=Label(cuadro_pe, text="¿Qué canción pertenece a Ha-ash”? ")
@@ -362,7 +331,6 @@ pregunta4=Label(cuadro_pe, text="¿Quién ganó el premio Grammy al mejor álbum
 pregunta4.config(bg="orange", fg="white")
 pregunta5=Label(cuadro_pe, text="¿Qué artista canta la canción “Por ti”?")
 pregunta5.config(bg="orange", fg="white")
-
 
 
 #Uso de combobox para desplegar opciones de respuesta
@@ -395,11 +363,6 @@ colocacion()
 
 
 
-
-
-
-
-
 #ventana secundaria musicales
 
 ventana_m=Toplevel(ventana)
@@ -426,9 +389,7 @@ nombre=Label(cuadro_m, text="Ingresa tu nombre:")
 nombre.config(bg="hot pink", fg="white")
 respuesta_nombre=Entry(cuadro_m)
 
-
-#Uso de combobox para desplegar opciones de respuesta
-
+#Uso de label para mostrar pregunta 
 pregunta1=Label(cuadro_m, text="¿De qué musical es la canción “For Forever”?")
 pregunta1.config(bg="hot pink", fg="white")
 pregunta2=Label(cuadro_m, text="¿Qué canción pertenece al musical “Be more chill”?")
@@ -441,7 +402,6 @@ pregunta4=Label(cuadro_m, text="¿Cuál personaje canta “One Last time” en H
 pregunta4.config(bg="hot pink", fg="white")
 pregunta5=Label(cuadro_m, text="¿Quién ganó el premio Tony al mejor musical en 2008?")
 pregunta5.config(bg="hot pink", fg="white")
-
 
 #Uso de combobox para desplegar opciones de respuesta
 opciones1_m=Combobox(cuadro_m,state="readonly",values=["Beetlejuice","Rent","Dear Evan Hansen","Mean Girls"])
@@ -471,8 +431,6 @@ fin_m.place(x=170, y=660)
 colocacion()
 
 
-
-
 #ventana secundaria rock
 ventana_r=Toplevel(ventana)
 ventana_r.title("Rock")
@@ -498,9 +456,7 @@ nombre=Label(cuadro_r, text="Ingresa tu nombre:")
 nombre.config(bg="darkviolet", fg="white")
 respuesta_nombre=Entry(cuadro_r)
 
-
-#Uso de combobox para desplegar opciones de respuesta
-
+#Uso de label para mostrar pregunta 
 pregunta1=Label(cuadro_r, text="¿De qué banda es la canción “Under Pressure”?")
 pregunta1.config(bg="darkviolet", fg="white")
 pregunta2=Label(cuadro_r, text="¿Qué canción pertenece a la banda “Aerosmith”?")
@@ -543,7 +499,5 @@ ventana_pi.iconify()
 ventana_pe.iconify()
 ventana_m.iconify()
 ventana_r.iconify()
-
-
 
 ventana.mainloop()
